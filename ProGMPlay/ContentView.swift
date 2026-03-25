89273894b5234269ddf3447d09f03bcb784f27de
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-03-25 15:20:58 +0200

    Initial Commit

---
+++ ProGMPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProGMPlay
+//
+//  Created by Maksim Kosharny on 25.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProGMPlay/ProGMPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProGMPlayApp.swift
+//  ProGMPlay
+//
+//  Created by Maksim Kosharny on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ProGMPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
